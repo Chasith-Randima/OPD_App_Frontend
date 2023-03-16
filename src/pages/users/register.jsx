a91457005947d50a82eec1e5739ea9187c8435fd
Chasith-Randima <chasith19@gmail.com>
2023-03-16 17:10:49 +0530

    initialized the app and sign up page and login page.

--- src/pages/users/register.jsx
+++ src/pages/users/register.jsx
@@ -54,7 +54,7 @@ const register = () => {
                   <input
                     type="text"
                     // value={email}
-                    // onChange={handleChange("email")}
+            
                     class="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
                     placeholder="Enter your email address"
                   />
