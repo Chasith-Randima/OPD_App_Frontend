40c8a923f56eafff8b4dbe414a266227f985c707
Chasith-Randima <chasith19@gmail.com>
2023-04-18 13:00:02 +0530

    temp page removed

--- src/pages/temp.jsx
+++ src/pages/temp.jsx
@@ -1,60 +1,60 @@
-const [alert, setAlert] = useState({
-  message: "",
-  error: false,
-  loading: false,
-  success: false,
-});
+// const [alert, setAlert] = useState({
+//   message: "",
+//   error: false,
+//   loading: false,
+//   success: false,
+// });
 
-const resetAlert = () => {
-  setAlert({ message: "", error: false, loading: false, success: false });
-};
+// const resetAlert = () => {
+//   setAlert({ message: "", error: false, loading: false, success: false });
+// };
 
-setAlert({ ...alert, loading: true });
+// setAlert({ ...alert, loading: true });
 
-if (data.status && data.status == "success") {
-  setAlert({
-    ...alert,
-    loading: false,
-    message: data.message,
-    error: false,
-    success: true,
-  });
+// if (data.status && data.status == "success") {
+//   setAlert({
+//     ...alert,
+//     loading: false,
+//     message: data.message,
+//     error: false,
+//     success: true,
+//   });
 
-  window.setTimeout(() => {
-    setAlert({ ...alert, success: false, message: "" });
-  }, 1500);
-}
+//   window.setTimeout(() => {
+//     setAlert({ ...alert, success: false, message: "" });
+//   }, 1500);
+// }
 
-//   ---in catch
+// //   ---in catch
 
-setAlert({
-  ...alert,
-  loading: false,
-  message: err.message,
-  error: true,
-  success: false,
-});
+// setAlert({
+//   ...alert,
+//   loading: false,
+//   message: err.message,
+//   error: true,
+//   success: false,
+// });
 
-<div className="flex justify-center">
-  {alert.error && (
-    <Message
-      message={alert.message}
-      // alert={"error"}
-      resetAlert={resetAlert}
-    />
-  )}
-  {alert.success && (
-    <Message
-      message={alert.message}
-      // alert={"success"}
-      resetAlert={resetAlert}
-    />
-  )}
-  {alert.loading && (
-    <Message
-      message={"Loading...Please Waite..."}
-      // alert={"loading"}
-      resetAlert={resetAlert}
-    />
-  )}
-</div>;
+// <div className="flex justify-center">
+//   {alert.error && (
+//     <Message
+//       message={alert.message}
+//       // alert={"error"}
+//       resetAlert={resetAlert}
+//     />
+//   )}
+//   {alert.success && (
+//     <Message
+//       message={alert.message}
+//       // alert={"success"}
+//       resetAlert={resetAlert}
+//     />
+//   )}
+//   {alert.loading && (
+//     <Message
+//       message={"Loading...Please Waite..."}
+//       // alert={"loading"}
+//       resetAlert={resetAlert}
+//     />
+//   )}
+// </div>;
