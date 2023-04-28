1e59518231fef1f5f71a7113d6c4adfa44333467
Chasith-Randima <chasith19@gmail.com>
2023-04-28 18:39:09 +0530

    connected profile update crud functions to front end.

--- actions/user.js
+++ actions/user.js
@@ -39,7 +39,6 @@ export const allUsers = (paramsData, token) => {
     // },
   })
     .then((response) => {
-      //   console.log(response.data);
       return response.data;
     })
     .catch((err) => {
